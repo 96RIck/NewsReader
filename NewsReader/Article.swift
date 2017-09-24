76e12fcd9a781e7bb676e752670f0d3350b9f8fa
96RIck <lesterblack@yahoo.com>
2017-09-24 09:44:45 +0800

    firstCommit

---
+++ NewsReader/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  NewsReader
+//
+//  Created by Rick on 2017/9/23.
+//  Copyright © 2017年 Rick. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+
+}
---
+++ NewsReader/ArticleCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleCell.swift
+//  NewsReader
+//
+//  Created by Rick on 2017/9/23.
+//  Copyright © 2017年 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
